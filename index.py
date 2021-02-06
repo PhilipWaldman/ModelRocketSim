ce579d17da2a131541679ab3b5ad7f4a8b1d11e7
Philip Waldman <philip.waldman01@gmail.com>
2021-02-06 21:36:24 +0100

    Added home page

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-from pages import page1, page2, page404
+from pages import page1, page2, page404, home_page
 
 all_pages = {
     '/page1': 'Page 1',
@@ -57,7 +57,9 @@ def display_page(pathname):
     :param pathname: The current pathname (the last part of the URL) of the page.
     :return: The page that should be at that pathname; otherwise a 404 page.
     """
-    if pathname == '/page1':
+    if pathname == '/':
+        return home_page.layout
+    elif pathname == '/page1':
         return page1.layout
     elif pathname == '/page2':
         return page2.layout
