cc88a325769665200629b33ec1a33fabfa2f9645
Philip Waldman <philip.waldman01@gmail.com>
2021-02-25 17:24:15 +0100

    Added margins

--- pages/page404.py
+++ pages/page404.py
@@ -1,5 +1,9 @@
 import dash_html_components as html
 
 layout = html.Div(
-    html.H3('This page does not exist, or does it...')
+    html.H3('This page does not exist, or does it...'),
+    style={
+        'margin-left': '2rem',
+        'margin-right': '2rem'
+    }
 )
