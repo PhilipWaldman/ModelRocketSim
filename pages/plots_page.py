7228188d6c6564f7c7b8b15b402b88dd3beda2dd
Philip Waldman <philip.waldman01@gmail.com>
2021-02-27 21:57:15 +0100

    Plots use hardcoded drag equation

--- pages/plots_page.py
+++ pages/plots_page.py
@@ -1,3 +1,5 @@
+import math
+
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
@@ -49,27 +51,31 @@ def altitude_time_graph(rocket_data, motor_data):
     motor_file = tc.thrust_files[0]
     if motor_data and 'motor_file' in motor_data.keys():
         motor_file = motor_data['motor_file']
-    motor_tc = tc.ThrustCurve(motor_file).thrust_curve_smooth(dt)
+    motor_tc = tc.ThrustCurve(motor_file).thrust_curve
 
     altitude = {}
     velocity = {}
     acceleration = {}
     y = 0
     v = 0
-    for t, F in motor_tc.items():
-        a = F / m - g
-        v += a * dt
-        y += v * dt
+    t = 0
+    for t1, F_thrust in motor_tc.items():
+        F = F_thrust - g * m - calc_drag_force(v)
+        a = F / m
+        v += a * (t1 - t)
+        y += v * (t1 - t)
         if y < 0:
             y = 0
             v = 0
-        altitude[t] = y
-        velocity[t] = v
-        acceleration[t] = a
-    t = max(motor_tc.keys())
+        altitude[t1] = y
+        velocity[t1] = v
+        acceleration[t1] = a
+        t = t1
+    dt = 0.01
     while y > 0:
         t += dt
-        a = - g
+        F = - g * m - calc_drag_force(v)
+        a = F / m
         v += a * dt
         y += v * dt
         altitude[t] = y
@@ -91,6 +97,16 @@ def altitude_time_graph(rocket_data, motor_data):
     fig_acc = px.line(x=acceleration.keys(), y=acceleration.values(), title='Acceleration-time')
     fig_acc.update_xaxes(range=x_range)
     fig_acc.update_layout(xaxis_title_text='Time (s)',
-                          yaxis_title_text='Altitude (m/s^2)')
+                          yaxis_title_text='Acceleration (m/s^2)')
 
     return fig_alt, fig_vel, fig_acc
+
+
+def calc_drag_force(v):
+    """ F_drag = 0.5 * Cd * ρ * v^2 * A \n
+    https://www.grc.nasa.gov/www/k-12/airplane/drageq.html
+
+    :param v: Velocity
+    :return: The drag force
+    """
+    return 0.5 * 0.37 * 1.205 * (v ** 2) * ((0.066 / 2) ** 2 * math.pi)
