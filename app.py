40d9b9a94f5ccfbab275aac13a63b22581b65210
Philip Waldman <philip.waldman01@gmail.com>
2021-04-17 16:48:52 +0200

    Added favicon and changed tab title

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import dash
 import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.FLATLY])
+app = dash.Dash(__name__,
+                title='WARP',
+                update_title='Loading...',
+                suppress_callback_exceptions=True,
+                external_stylesheets=[dbc.themes.FLATLY])
 server = app.server
